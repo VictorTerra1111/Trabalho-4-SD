323eaf25d502ce249f51976d06d10202371a2d20
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-11 07:59:08 -0300

    Update fpu.sv

--- src/fpu.sv
+++ src/fpu.sv
@@ -1,6 +1,6 @@
 module fpu(
-    input logic [31:0]op_a_in,  // expoente
-    input logic [31:0]op_b_in, // mantica
+    input logic [31:0]op_A_in,  // expoente
+    input logic [31:0]op_B_in, // mantica
 
     input logic clock100KHz,
     input logic reset,
@@ -9,52 +9,41 @@ module fpu(
     output logic [3:0] status_out, 
     output logic flags_out
 );
-/*
-Para determinar o valor de X, utilize a fórmula X = [8 (+/-) ∑b mod 4], onde ∑b
-representa a soma de todos os dígitos de seu número de matrícula (base 10) e mod 4
-representa o resto da divisão inteira por 4. O sinal + ou - é determinado por seu dígito
-verificador do número de matrícula: + se for ímpar, - se for par. O valor de Y é dado por
-Y = 32 - X.
-24103806-6 
-somatório: 31 mod 4 = 3
-X = 8 - 3 = 5 expoente
-Y = 32 - 5 = 27 mantissa VERIFICAR TALVEZ 26
-
-[1][00000][00000000...] 
-1 - sinal (31)
-5 - expoente (30-26)
-27 - mantissa (0-25)
-
-*/
+    
     typedef enum logic {
         EXACT, //deu tudo certo
-        INEXACT, //arredondamnento
-        OVERFLOW, //passou do tamanho
-        UNDERFLOW //menor que o limite inferior
+        INEXACT, //arredondou
+        OVERFLOW, //passou do tamanho (62)
+        UNDERFLOW //menor que o 1
     } states_t; 
 
     states_t c_state;
 
-    logic [4:0] exp1, exp2, exp_out;
-    logic [26:0] mant1, mant2, mant_out;
+    logic [4:0] expA, expB, exp_out;
+    logic [26:0] mantA, mantB, mant_out;
+    logic sinal_out, sinalA, sinalB;
     
     always @(posedge clock100KHz, negedge reset) begin
         if(reset) begin
             flags_out <= 0;
         end else begin
-            exp1 <= op_a_in[30:26];
-            exp2 <= op_b_in[30:26]; 
+            expA <= op_A_in[30:25];
+            expB <= op_B_in[30:25]; 
 
-            mant1 <= op_a_in[25:0];
-            mant2 <= op_b_in[25:0]; 
+            mantA <= op_A_in[24:0];
+            mantB <= op_B_in[24:0]; 
+            sinalA <= op_A_in[31];
+            sinalB <= op_B_in[31]; 
 
-            if(exp1 > exp2) begin 
-                exp_out <= exp2;
-            end 
-            else if(exp1 =< exp2) begin
-                exp_out <= exp1;
+            if(expA == expB && sinalA == sinalB) begin
+                mant_out <= mantA + mantB; 
+            end else if (mantA < mantB) begin
+                mant_out <= mantB - mantA;
+                sinal_out <= sinalB; 
+            end else if (mantB < mantA) begin
+                mant_out <= mantA - mantB; 
+                sinal_out <= sinalA;
             end
-            
             case c_state
                 EXACT: 
                 INEXACT: 
