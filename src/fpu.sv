2378828d3085464941923a458fa33d470fa9eb39
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-11 14:36:48 -0300

    Update fpu.sv

--- src/fpu.sv
+++ src/fpu.sv
@@ -75,70 +75,69 @@ always @(posedge clock100KHz or negedge reset) begin
     end else begin
         current_state <= next_state;
     end else begin
-    case (current_state)
-        ALIGN: begin
-            if (expA > expB) begin
-                exp_diff = expA - expB;
-                mantB_shifted = mantB >> exp_diff;
-                mantA_shifted = mantA;
-                exp_result = expA;
-            end else if (expB > expA) begin
-                exp_diff = expB - expA;
-                mantA_shifted = mantA >> exp_diff;
-                mantB_shifted = mantB;
-                exp_result = expB;
-            end else begin
-                mantA_shifted = mantA;
-                mantB_shifted = mantB;
-                exp_result = expA;
+        case (current_state)
+            ALIGN: begin
+                if (expA > expB) begin
+                    exp_diff = expA - expB;
+                    mantB_shifted = mantB >> exp_diff;
+                    mantA_shifted = mantA;
+                    exp_result = expA;
+                end else if (expB > expA) begin
+                    exp_diff = expB - expA;
+                    mantA_shifted = mantA >> exp_diff;
+                    mantB_shifted = mantB;
+                    exp_result = expB;
+                end else begin
+                    mantA_shifted = mantA;
+                    mantB_shifted = mantB;
+                    exp_result = expA;
+                end
             end
-        end
-
-        OPERATE: begin
-            if (sinalA == sinalB) begin
-                mant_result_temp = mantA_shifted + mantB_shifted;
-                sinal_result = sinalA;
-            end else begin
-                if (mantA_shifted >= mantB_shifted) begin
-                    mant_result_temp = mantA_shifted - mantB_shifted;
+    
+            OPERATE: begin
+                if (sinalA == sinalB) begin
+                    mant_result_temp = mantA_shifted + mantB_shifted;
                     sinal_result = sinalA;
                 end else begin
-                    mant_result_temp = mantB_shifted - mantA_shifted;
-                    sinal_result = sinalB;
+                    if (mantA_shifted >= mantB_shifted) begin
+                        mant_result_temp = mantA_shifted - mantB_shifted;
+                        sinal_result = sinalA;
+                    end else begin
+                        mant_result_temp = mantB_shifted - mantA_shifted;
+                        sinal_result = sinalB;
+                    end
                 end
             end
-        end
-
-        NORMALIZE: begin
-            if (mant_result_temp[26]) begin
-                mant_result_temp = mant_result_temp >> 1;
-                exp_result = exp_result + 1;
-            end else begin
-                while (mant_result_temp[25] == 0 && exp_result > 0) begin
-                    mant_result_temp = mant_result_temp << 1;
-                    exp_result = exp_result - 1;
+    
+            NORMALIZE: begin
+                if (mant_result_temp[26]) begin
+                    mant_result_temp = mant_result_temp >> 1;
+                    exp_result = exp_result + 1;
+                end else begin
+                    while (mant_result_temp[25] == 0 && exp_result > 0) begin
+                        mant_result_temp = mant_result_temp << 1;
+                        exp_result = exp_result - 1;
+                    end
                 end
+                mant_result = mant_result_temp[24:0];
             end
-            mant_result = mant_result_temp[24:0];
-        end
-
-        ROUND: begin
-            // arredondar
-            status_reg = EXACT;
-        end
-
-        PACK: begin
-            if (exp_result > 6'b111111) begin
-                status_reg = OVERFLOW;
-            end else if (exp_result == 0 && mant_result != 0) begin
-                status_reg = UNDERFLOW;
+    
+            ROUND: begin
+                // arredondar
+                status_reg = EXACT;
             end
-            data_out = {sinal_result, exp_result, mant_result};
-            status_out = status_reg;
-        end
-        default: ;
-    endcase
+    
+            PACK: begin
+                if (exp_result > 6'b111111) begin
+                    status_reg = OVERFLOW;
+                end else if (exp_result == 0 && mant_result != 0) begin
+                    status_reg = UNDERFLOW;
+                end
+                data_out = {sinal_result, exp_result, mant_result};
+                status_out = status_reg;
+            end
+            default: ;
+        endcase
     end
 end
-
 endmodule
