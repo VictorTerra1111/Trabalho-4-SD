8d00690e3adb66d281e1fcd85c2e0fc00906e902
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-13 11:52:59 +0000

    changed base tb

--- sim/tb.sv
+++ sim/tb.sv
@@ -1,37 +1,36 @@
 `timescale 1ns/1ps
 
 module tb_fpu;
+
     logic [31:0]op_A_in; 
     logic [31:0]op_B_in; 
-
     logic clock100KHz;
     logic reset;
-
     logic [31:0] data_out;
     logic [3:0] status_out; 
     logic flags_out;
 
-fpu dut (
-    .op_A_in(op_A_in), 
-    .op_B_in(op_B_in), 
-    .clock100KHz(clock100KHz),
-    .reset(reset),
-    .data_out(data_out),
-    .status_out(status_out), 
-    .flags_out(flags_out)
-);
+    fpu dut (
+        .op_A_in(op_A_in), 
+        .op_B_in(op_B_in), 
+        .clock100KHz(clock100KHz),
+        .reset(reset),
+        .data_out(data_out),
+        .status_out(status_out), 
+        .flags_out(flags_out)
+    );
 
-initial clock100KHz = 0;
-always #50 clock100KHz = ~clock100KHz;
+    initial clock100KHz = 0;
+    always #5 clock100KHz = ~clock100KHz;
 
-initial begin
-    reset = 0;
-    #10;
-    reset = 1;
-    #10;
-    reset = 0;
-    
-    #100;
-    $finish;
-end
+    initial begin
+        reset = 0;
+        #10;
+        reset = 1;
+        #10;
+        reset = 0;
+        
+        #100;
+        $finish;
+    end
 endmodule
